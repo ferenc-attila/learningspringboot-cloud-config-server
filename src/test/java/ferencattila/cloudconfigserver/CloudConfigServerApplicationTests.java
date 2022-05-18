abcfbc683da05826758f1146e27784385b2fe945
ferenc-attila <attila.ferenc.80@gmail.com>
2022-05-18 20:17:14 +0200

    Initial commit

---
+++ src/test/java/ferencattila/cloudconfigserver/CloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ferencattila.cloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
